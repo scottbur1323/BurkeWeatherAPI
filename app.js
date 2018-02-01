79deece4a8db60b0cbe6b859b20303836b272b0a
Chance Burke <burke@Chances-MacBook-Pro.local>
2018-02-01 15:33:46 -0700

    finished a basic functioning webApp

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
-var poop = "poop"
-console.log(poop)
+let dropDown = document.querySelector(".citySelect")
+let baseWeatherURL = "http://api.openweathermap.org/data/2.5/weather?id="
+let weatherKey = "&APPID=262303768614f8e18789b312097675b8"
+let submitText = document.querySelector("h5")
+dropDown.addEventListener("change", getCurrentWeather)
+
+
+function getCurrentWeather() {
+  var cityURL = dropDown.value
+  var fullWeatherURL = baseWeatherURL + cityURL + weatherKey
+  fetch(fullWeatherURL)
+    .then(function(response){
+      return response.json()
+    }).then(function(data) {
+      var tempC = parseInt(data.main.temp - 273)
+      var tempF = parseInt(tempC * (9/5) + 32)
+      submitText.innerHTML = "Temp in Celsius: " + tempC + ".  Temp in Fahrenheit: " + tempF
+      var tempC = data.main.temp - 273
+      var tempF = tempC * (9/5) + 32
+    }).catch(function(error) {
+      console.log(error)
+    })
+}
