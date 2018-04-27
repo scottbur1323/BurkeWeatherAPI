2b226fe67e038a9627479a923f938d0de1bd565f
Chance Burke <burke@Chances-MacBook-Pro.local>
2018-04-26 21:29:11 -0600

    update

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 let dropDown = document.querySelector(".citySelect")
 let baseWeatherURL = "https://api.openweathermap.org/data/2.5/weather?id="
-let weatherKey = "&APPID=262303768614f8e18789b312097675b8"
 let submitText = document.querySelector("h5")
+let weatherKey = "262303768614f8e18789b312097675b8"
 dropDown.addEventListener("change", getCurrentWeather)
-
 
 function getCurrentWeather() {
   var cityURL = dropDown.value
   var fullWeatherURL = baseWeatherURL + cityURL + weatherKey
+
   fetch(fullWeatherURL)
     .then(function(response){
       return response.json()
